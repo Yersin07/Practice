3ea009e0daf85f618fab91e0b71f85f7dc3ac67f
Ersin <y_beisengali@kbtu.kz>
2026-06-24 19:11:31 +0300

    Add basic functions for greeting and multiplication

---
+++ Practice3/functions/basic_functions.py
@@ -0,0 +1,23 @@
+#1
+def greet():
+    print("Hello!")
+
+greet()
+
+#2
+def welcome(name):
+    print("Welcome,", name)
+
+welcome("Yers")
+
+#3
+def multiply(a, b):
+    print(a * b)
+
+multiply(5, 4)
+
+#4
+def course():
+    print("Python Programming")
+
+course()
